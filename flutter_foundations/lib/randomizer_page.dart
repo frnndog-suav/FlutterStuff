4a0ceae0ecb620c7d2284754bdac13d1dfc942df
Fernando Goia <fnagoya_hr2@hotmail.com>
2022-10-25 11:41:42 -0300

    Riverpod

--- flutter_foundations/lib/randomizer_page.dart
+++ flutter_foundations/lib/randomizer_page.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_foundations/randomizer_change_notifier.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RandomizerPage extends StatelessWidget {
+import 'main.dart';
+
+class RandomizerPage extends ConsumerWidget {
   const RandomizerPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final randomizer = ref.watch(randomizerProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Randomizer"),
       ),
-      body: Center(child:
-          Consumer<RandomizerChangeNotifier>(builder: (context, value, child) {
-        return Text(
-          value.generatedNumber?.toString() ?? "Generate a number",
-          style: TextStyle(fontSize: 42),
-        );
-      })),
+      body: Center(
+          child: Text(
+        randomizer.generatedNumber?.toString() ?? "Generate a number",
+        style: TextStyle(fontSize: 42),
+      )),
       floatingActionButton: FloatingActionButton.extended(
         label: Text("Generate"),
         onPressed: () {
-          context.read<RandomizerChangeNotifier>().generateRandomNumber();
+          ref.read(randomizerProvider).generateRandomNumber();
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
